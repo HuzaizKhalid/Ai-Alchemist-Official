96af2159135c632349ee393fbff09b8dece19e28
Huzaiz Khalid <huzaizwebdev@gmail.com>
2025-10-19 23:48:44 +0500

    updated

--- app/dashboard/calendar/co2-risk/page.tsx
+++ app/dashboard/calendar/co2-risk/page.tsx
@@ -1,5 +1,13 @@
-import CO2RiskDashboard from "@/components/CO2RiskDashboard";
+import { CO2RiskDashboard } from "@/components/CO2RiskDashboard";
+import Background from "@/components/Background";
 
 export default function CO2RiskStatisticsPage() {
-  return <CO2RiskDashboard />;
+  return (
+    <div className="min-h-screen relative">
+      <Background />
+      <div className="relative z-10 container max-w-4xl mx-auto px-4 py-8">
+        <CO2RiskDashboard totalCO2Emissions={1000} timeFilter="D" />
+      </div>
+    </div>
+  );
 }
